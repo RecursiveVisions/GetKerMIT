47e58e06a83026b05634278b3648ea4c7fa4a9c4
RecursiveVisions <184334431+RecursiveVisions@users.noreply.github.com>
2026-03-05 06:06:19 -0500

    Minor fixes
    - rename "Freeze animated gifs" to "Disable animations"
    - add debug functionality
    - disable roses animation if "Disable animations" is checked

--- js/application.js
+++ js/application.js
@@ -1,4 +1,6 @@
+let gameManager = null;
+
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  gameManager = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
--- js/index.js
+++ js/index.js
@@ -17,24 +17,34 @@ function onOutlinesCheckToggle() {
 }
 
 
-function freezeGifs(enabled) {
+function disableAnimations(enabled) {
 	let tiles = document.querySelectorAll(".tile .tile-inner");
 	if (enabled) {
+		// add tile-N-still class
 		for (let i = 0; i < tiles.length; i++) {
 			if (parseInt(tiles[i].textContent) > 64) {
 				tiles[i].parentElement.classList.add(`tile-${tiles[i].textContent}-still`);
 			}
 		}
+
+		// hide roses overlay
+		let rosesOverlay = document.getElementById("rose-overlay");
+        rosesOverlay.style.display = "none";
 	} else {
+		// remove tile-N-still class
 		for (let i = 0; i < tiles.length; i++) {
 			if (parseInt(tiles[i].textContent) > 64) {
 				tiles[i].parentElement.classList.remove(`tile-${tiles[i].textContent}-still`);
 			}
 		}
+
+		// hide roses overlay
+		let rosesOverlay = document.getElementById("rose-overlay");
+        rosesOverlay.style.display = "block";
 	}
 }
 
 function onFreezeCheckToggle() {
 	var freezeCheck = document.getElementById("freeze-check");
-	freezeGifs(freezeCheck.checked);
+	disableAnimations(freezeCheck.checked);
 }
